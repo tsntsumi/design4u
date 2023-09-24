290904782d909545f69b72621cd5346c0be23538
tsntsumi <tsutsumi.kikuo@gmail.com>
2023-09-24 15:36:51 +0900

    remove firebase admin initializeApp procedure

--- libs/firebase.js
+++ libs/firebase.js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 
-export const admin = require('firebase-admin')
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,24 +20,5 @@ export const firebaseConfig = {
 export const app = initializeApp(firebaseConfig)
 //export const analytics = getAnalytics(app)
 // export const db = getFirestore(app)
-
-const serviceAccount = {
-  type: 'service_account',
-  project_id: 'alizza-ideal-web',
-  private_key_id: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
-  client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID,
-  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-  token_uri: 'https://oauth2.googleapis.com/token',
-  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-  client_x509_cert_url:
-    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-38g70%40alizza-ideal-web.iam.gserviceaccount.com',
-  universe_domain: 'googleapis.com',
-}
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-})
 
 export default app
