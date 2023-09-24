c7358d038dc02ae09119751caa343efdb078c3fe
tsntsumi <tsutsumi.kikuo@gmail.com>
2023-09-24 15:21:39 +0900

    Fix secret file miss location

--- libs/firebase.js
+++ libs/firebase.js
@@ -23,7 +23,20 @@ export const app = initializeApp(firebaseConfig)
 //export const analytics = getAnalytics(app)
 // export const db = getFirestore(app)
 
-const serviceAccount = require(`@/libs/alizza-ideal-web-firebase-adminsdk-serviceaccountkey.json`)
+const serviceAccount = {
+  type: 'service_account',
+  project_id: 'alizza-ideal-web',
+  private_key_id: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+  client_email: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_ID,
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-38g70%40alizza-ideal-web.iam.gserviceaccount.com',
+  universe_domain: 'googleapis.com',
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
