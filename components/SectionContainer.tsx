cd395ac5d343de2f64b076ec42c74411390af7ce
tsntsumi <tsutsumi.kikuo@gmail.com>
2023-09-28 21:02:00 +0900

    Update layout of legal and policy pages

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -2,10 +2,13 @@ import { ReactNode } from 'react'
 
 interface Props {
   children: ReactNode
+  className: string
 }
 
-export default function SectionContainer({ children }: Props) {
+export default function SectionContainer({ children, className }: Props) {
   return (
-    <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</section>
+    <section className={`${className} mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0`}>
+      {children}
+    </section>
   )
 }
